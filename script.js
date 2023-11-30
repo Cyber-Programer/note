15f53aa325f77ae468761428132b2f220934b951
MD siFaT isLaM <125746506+Cyber-Programer@users.noreply.github.com>
2023-11-30 11:31:03 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const notesContainer = document.querySelector('.notes');
+const createBtn = document.querySelector('.btn');
+
+function showNotes() {
+  const savedNotes = localStorage.getItem('notes');
+  if (savedNotes) {
+    notesContainer.innerHTML = savedNotes;
+  }
+}
+
+function updateNote() {
+  localStorage.setItem('notes', notesContainer.innerHTML);
+}
+
+showNotes();
+
+createBtn.addEventListener('click', function () {
+  let inputBox = document.createElement('p');
+  let img = document.createElement('img');
+
+  inputBox.id = 'notes';
+  inputBox.setAttribute('contenteditable', 'true');
+  img.src = '/images/delete.png';
+  img.id = 'delet-ico';
+
+  inputBox.appendChild(img);
+  notesContainer.appendChild(inputBox);
+
+  updateNote(); // Save the updated notes after adding a new note
+});
+
+notesContainer.addEventListener('click', function (e) {
+  if (e.target.tagName === 'IMG') {
+    e.target.parentElement.remove();
+    updateNote();
+  }
+});
+
+notesContainer.addEventListener('keyup', function (e) {
+  if (e.target.tagName === 'P') {
+    updateNote();
+  }
+});
